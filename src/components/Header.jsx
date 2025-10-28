d4cddacd710eb52d0c2a94fac2aa8f78999c566d
ReimboyevShohrux <shohruxreimboyev4@gmail.com>
2025-10-28 22:01:30 +0300

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,22 @@ import logo from '../assets/svg/logo.svg'
 
 const Header = () => {
   return (
+    <header>
+      <div className="container header_container">
+        <a href="#">
+        <img src={logo} alt="Logo" className="logo" />
+        </a>
 
+        <nav className="nav_links">
+          <a href="#features">Features</a>
+          <a href="#about">About us</a>
+          <a href="#pricing">Pricing</a>
+          <a href="#feedback">Feedback</a>
+        </nav>
+
+        <button className="btn_demo">Request a demo</button>
+      </div>
+    </header>
   )
 }
 
